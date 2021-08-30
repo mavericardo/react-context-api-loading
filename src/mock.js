5a7ed70caf8ea14889a81ef0ceaf6fa8385cb3dd
Marcos Ricardo <mricardoalves99@gmail.com>
2021-08-30 11:06:00 -0300

    understanding the React Context API: Creating a Loading Component

---
+++ src/mock.js
@@ -0,0 +1,62 @@
+export const users = [
+    {
+        id: 1,
+        first_name: "Marcos",
+        last_name: "Ricardo",
+        email: "marcos.email@gmail.com",
+        gender: "Male"
+    },
+    {
+        id: 2,
+        first_name: "Victor",
+        last_name: "Aquino",
+        email: "victor.aquino.email@gmail.com",
+        gender: "Male"
+    },
+    {
+        id: 3,
+        first_name: "Gabriel",
+        last_name: "Augusto",
+        email: "gabriel.augusto.email@gmail.com",
+        gender: "Male"
+    }
+];
+
+export const departaments = [
+    {
+        "id":1,
+        "name":"Training"
+    },
+    {
+        "id":2,
+        "name":"Legal"
+    },
+    {
+        "id":3,
+        "name":"Human Resources"
+    },
+    {
+        "id":4,
+        "name":"Services"
+    },
+    {
+        "id":5,
+        "name":"Support"
+    },
+    {
+        "id":6,
+        "name":"Sales"
+    },
+    {
+        "id":7,
+        "name":"Training"
+    },
+    {
+        "id":8,
+        "name":"Legal"
+    },
+    {
+        "id":10,
+        "name":"Research and Development"
+    }
+];
